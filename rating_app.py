d121604405b13f852d899a8e77321ce9b2cb3fa4
Bhavya Motiyani <bhavyamotiyani68@gmail.com>
2026-07-15 17:36:37 +0530

    Update rating_app.py

--- rating_app.py
+++ rating_app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import tensorflow as tf
 import pickle
@@ -41,4 +42,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT", 5000)),debug=False)
